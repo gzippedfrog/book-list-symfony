a2dfe8770b75d10ee2ea12164020cb99a5ae99cb
gzippedfrog <gzippedfrog@outlook.com>
2023-07-16 02:08:53 +0300

    updated parser command

--- src/Command/ParseBooksCommand.php
+++ src/Command/ParseBooksCommand.php
@@ -15,79 +15,80 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'app:parse-books',
-    description: 'Parses books info from a JSON',
+    description: 'Parses books info from a JSON file',
 )]
 class ParseBooksCommand extends Command
 {
-    protected EntityManagerInterface $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(
+        protected readonly EntityManagerInterface $entityManager,
+    )
     {
         parent::__construct();
-
-        $this->entityManager = $entityManager;
     }
 
     protected function configure(): void
     {
         $this
-            ->addArgument('source', InputArgument::REQUIRED, 'path to the json file')
-            ->addOption('url', null, InputOption::VALUE_NONE, 'use url instead of file path');
+            ->addArgument('file_path', InputArgument::REQUIRED, 'path to the json file');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
+        $file_path = $input->getArgument('file_path');
 
-        $source = $input->getArgument('source');
+        $books_added = 0;
+        $categories_added = 0;
+        $jsonData = json_decode(file_get_contents($file_path), true);
+        $em = $this->entityManager;
 
-        if ($source) {
-            $jsonData = json_decode(file_get_contents($source), true);
-            $em = $this->entityManager;
+        foreach ($jsonData as $bookData) {
+            $book = $em->getRepository(Book::class)->findOneBy([
+                'title' => $bookData['title'],
+                'authors' => [implode(',', $bookData['authors'])]
+            ]);
 
-            foreach ($jsonData as $bookData) {
-                $book = $em->getRepository(Book::class)->findOneBy(['title' => $bookData['title']]);
+            if (!$book) {
+                $book = new Book();
+                $book->setTitle($bookData['title']);
+                $book->setIsbn($bookData['isbn'] ?? null);
 
-                if (!$book) {
-                    $book = new Book();
-                    $book->setTitle($bookData['title']);
-                    $book->setIsbn($bookData['isbn'] ?? null);
+                $date = $bookData['publishedDate']['$date'] ?? null;
+                $book->setPublishedDate($date ? new \DateTime($date) : null);
 
-                    $date = $bookData['publishedDate']['$date'] ?? null;
-                    $book->setPublishedDate($date ? new \DateTime($date) : null);
+                $book->setThumbnailUrl($bookData['thumbnailUrl'] ?? null);
+                $book->setAuthors($bookData['authors']);
+                $book->setStatus($bookData['status']);
+                $book->setShortDescription($bookData['shortDescription'] ?? null);
+                $book->setLongDescription($bookData['longDescription'] ?? null);
+                $book->setPageCount($bookData['pageCount'] > 0 ? $bookData['pageCount'] : null);
 
-                    $book->setThumbnailUrl($bookData['thumbnailUrl'] ?? null);
-                    $book->setAuthors($bookData['authors']);
-                    $book->setStatus($bookData['status']);
-                    $book->setShortDescription($bookData['shortDescription'] ?? null);
-                    $book->setLongDescription($bookData['longDescription'] ?? null);
-                    $book->setPageCount($bookData['pageCount'] > 0 ? $bookData['pageCount'] : null);
-
-                    $this->entityManager->persist($book);
-                }
-
-                foreach ($bookData['categories'] as $categoryName) {
-                    $category = $em->getRepository(Category::class)->findOneBy(['name' => $categoryName]);
-
-                    if (!$category) {
-                        $category = new Category();
-                        $category->setName($categoryName);
-
-                        $this->entityManager->persist($category);
-
-                    }
-
-                    if (!$category->getBooks()->contains($book)) {
-                        $category->addBook($book);
-                    }
-                }
-
-                $this->entityManager->flush();
-
+                $this->entityManager->persist($book);
+                $books_added++;
             }
-            return Command::SUCCESS;
+
+            if (empty($bookData['categories'])) {
+                $bookData['categories'] = ['New'];
+            }
+
+            foreach ($bookData['categories'] as $categoryName) {
+                $category = $em->getRepository(Category::class)->findOneByName($categoryName);
+
+                if (!$category) {
+                    $category = new Category();
+                    $category->setName($categoryName);
+
+                    $this->entityManager->persist($category);
+                    $categories_added++;
+                }
+
+                $category->addBook($book);
+            }
+
+            $this->entityManager->flush();
 
         }
-        return Command::FAILURE;
+        $io->success("Command successful\nBooks added: $books_added\nCategories added: $categories_added");
+        return Command::SUCCESS;
     }
 }
